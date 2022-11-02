f500c43a6fa0b1c0fd798c48f30411a587e47837
praveen <praveensunkavalli9@gmail.com>
2022-11-02 17:01:32 +0530

    firstcommit

---
+++ test.py
@@ -0,0 +1,9 @@
+n=1
+while(n<=10):
+    
+    a=n*5
+  
+    print('5*{0}={1}'.format(n,a),end=" "),
+    print("samosa")
+    #print("tintava")
+    n=n+1
---
+++ triangle.py
@@ -0,0 +1,7 @@
+# calculate the area of a traingle
+
+b=float(input("enter the breadth of the triangle: "))
+h=float(input("enter the height of the triangle: "))
+area=1/2*b*h
+print(area)
+
